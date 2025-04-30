eea94554342347a81eea49a4384dbe72947b6917
TristanTico <125488520+TristanTico@users.noreply.github.com>
2025-04-29 18:02:37 -0600

    Fix bugs

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
-import CV_SPANISH from "../../assets/Hoja_de_vida_Tristan Lopez.pdf";
+import CV_SPANISH from "../../assets/Hoja_de_Vida_Tristan_Lopez.pdf";
 
 export const Home = () => {
   const [open, setOpen] = useState(false);
